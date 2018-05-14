aa8e618393a3a6d5cf5ec7ffb9324e4dba086069
Elcoosp <elcoosp@gmail.com>
2018-05-14 19:43:09 +0200

    feat(index): Main programm, init the cli, prompt questions, create the template and write it

---
+++ src/index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const inquirer = require('inquirer')
+const readMeQuestions = require('./readMeQuestions')
+const templateReadMe = require('./templateReadMe')
+const fs = require('fs')
+
+const promptAndProcess = readMeQuestions => async () => {
+  const responses = await inquirer.prompt(readMeQuestions)
+  const readme = templateReadMe(responses)
+
+  const writeStream = fs.createWriteStream(`${process.cwd()}/README.md`)
+  writeStream.write(readme)
+  writeStream.end()
+}
+
+program
+  .version('0.1.0')
+  .command('init')
+  .action(promptAndProcess(readMeQuestions))
+
+program.parse(process.argv)
